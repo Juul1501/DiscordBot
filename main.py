64aff38c6958adf013ee874ebcdbc9d7d8cb389c
Juul Cox <36227603+Juul1501@users.noreply.github.com>
2018-05-26 22:33:17 +0200

    added web search google and bing started with giphy search

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
-<<<<<<< HEAD
-=======
-import selenium.webdriver as webdriver
-from selenium.webdriver.common.keys import Keys
 
->>>>>>> b06638ceeee2bad9f5356eda910af4187ed0551d
+import urllib
+import urllib.request
 
 bot = commands.Bot(";")
 
@@ -15,39 +12,80 @@ async def on_ready():
     print("Bot online")
 
 @bot.command(pass_context=True)
-<<<<<<< HEAD
-async def cookie(ctx):
-    await bot.say("koekje")
-=======
-async def search(ctx, arg1):
-    get_result(arg1)
-    imgurl = links
-    await bot.say("imgurl")
+async def search(ctx, *, query):
+    channel = ctx.message.channel                                           #Dit is de channel waarin de text post is gemaakt
+    if not get_result_bing(query):                                          #Als bing kut doet...
+        get_result_google_thumb(query)                                      #doe dan google (al zijn dat alleen thumbnails)
+    await bot.send_file(channel, "img.jpg", content=query)
+    print ("search command used")
 
+@bot.command(pass_context=True)
+async def fn(ctx, *, query):
+    channel = ctx.message.channel
+    get_result_giphy(query)
+    await bot.send_file(channel, "img.gif", content=query)
 
+def get_result_google_thumb(search_term):
+    try:    #Probeer dit indien het foutgaat geef False terug anders True (zorgt dat de bot niet breekt)
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' #Deze header zorgt dat google niet gaat klagen dat we een bot zijn
 
->>>>>>> b06638ceeee2bad9f5356eda910af4187ed0551d
+        headers={'User-Agent':user_agent,}                                  #Zet de header in elkaar
+        url = "http://www.google.com/search?tbm=isch&q="\
+         + search_term.replace(" ", "+")                                    #Vervang spaties met plus-tekens zodat google niet zeurt.
 
+        request=urllib.request.Request(url,None,headers)                    #Stuur een http request
+        response = urllib.request.urlopen(request)                          #Vang het antwoord (locked het programma totdat het antwoord verkregen is, kan kapot gaan bij slechte verbinding!)
+        data = str(response.read())                                         #Lees het antwoord en zet het van bytecode om naar een tekst
+        image_url = data.split('<a href="/url?q=')[1].split("src=")[1].split('"')[1]    #Haal de eerste thumbnail eruit
 
-def get_result(search_term):
-    url = "https://images.google.com/"
-    browser = webdriver.Firefox()
-    browser.get(url)
-    search_box = browser.find_element_by_id("lst-ib")
-    search_box.send_keys(search_term)
-    search_box.send_keys(Keys.RETURN)
-    links = browser.find_element_by_xpath("//a")
+        urllib.request.urlretrieve(image_url, "img.jpg")
+        return True
+    except Exception:
+        return False
 
-    results = []
+def get_result_bing(search_term):
+    try:    #Probeer dit indien het foutgaat geef False terug anders True (zorgt dat de bot niet breekt)
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' #Deze header zorgt dat google niet gaat klagen dat we een bot zijn
 
-    for link in links:
-        href = link.get_attribute("href")
-        print(href)
-        results.append(href)
-    browser.close()
-    return results
-    
-    
+        headers={'User-Agent':user_agent,}                                  #Zet de header in elkaar
 
+        url = "https://www.bing.com/images/search?q="\
+            + search_term.replace(" ", "+")                                 #Zoekveld voor bing, vervang spaties voor plustekens
+
+        request=urllib.request.Request(url,None,headers)                    #Stuur een http request
+        response = urllib.request.urlopen(request)                          #Vang het antwoord (locked het programma totdat het antwoord verkregen is, kan kapot gaan bij slechte verbinding!)
+        data = str(response.read())                                         #Lees het antwoord en zet het van bytecode om naar een tekst
+        with open('output.txt','w') as file:
+            file.write(data)
+        image_url = data.split(".jpg&quot;,&quot;turl")[1].split("&quot;:&quot;")[-1]+".jpg" #Haal de afbeelding eruit. Dit gebeurt redelijk arbitrair :P
+
+        urllib.request.urlretrieve(image_url, "img.jpg")
+        return True
+    except Exception:
+        return False
+
+def get_result_giphy(search_term):
+    try:
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+
+        headers={'User-Agent':user_agent,}
+
+        url = "https://giphy.com/search/"\
+        + search_term.replace(" ", "-")
+
+        request=urllib.request.Request(url,None,headers)
+        response = urllib.request.urlopen(request)
+        
+        data = str(response.read())
+        with open('outputgiphy.txt','w') as file:
+            file.write(data)
+
+        image_url = data.split('<a class = "_gifImage_1mf53_41 _gifLink_1mf53_51')[1].split("src=")[1].split('"')[1]+".gif"
+
+        urllib.request.urlretrieve(image_url, "img.gif")
+        
+        return True
+    except Exception:
+        return False
 
 bot.run("Mzk2MjUwNzY1MTI5NDE2NzA4.Deg_6A.NdjUG1wHUsogDdAX9K9aUPJguI0")
--- websearch.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import urllib.parse
-from urllib.request import Request, urlopen
-url = 'https://fortnitetracker.com/profile/pc/Juul1501'
-f = urllib.request.urlopen(url)
-print(f.read().decode('utf-8'))
-
-
-
