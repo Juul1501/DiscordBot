47e40a27ef0d00ef8b073c1f2aec5f7db8eb6945
Juul Cox <Juulcox00@Gmail.com>
2018-05-28 12:02:03 +0200

    added bot join voice channel command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
+import asyncio
 
 import urllib
 import urllib.request
@@ -24,19 +25,24 @@ async def fn(ctx, *, query):
     channel = ctx.message.channel
     get_result_giphy(query)
     await bot.send_file(channel, "img.gif", content=query)
+@bot.command(pass_context=True)
+async def join(ctx): 
+    author = ctx.message.author
+    channel = author.voice_channel 
+    await bot.join_voice_channel(channel) 
 
 def get_result_google_thumb(search_term):
-    try:    #Probeer dit indien het foutgaat geef False terug anders True (zorgt dat de bot niet breekt)
+    try:    
         user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' #Deze header zorgt dat google niet gaat klagen dat we een bot zijn
 
         headers={'User-Agent':user_agent,}                                  #Zet de header in elkaar
         url = "http://www.google.com/search?tbm=isch&q="\
          + search_term.replace(" ", "+")                                    #Vervang spaties met plus-tekens zodat google niet zeurt.
 
-        request=urllib.request.Request(url,None,headers)                    #Stuur een http request
-        response = urllib.request.urlopen(request)                          #Vang het antwoord (locked het programma totdat het antwoord verkregen is, kan kapot gaan bij slechte verbinding!)
-        data = str(response.read())                                         #Lees het antwoord en zet het van bytecode om naar een tekst
-        image_url = data.split('<a href="/url?q=')[1].split("src=")[1].split('"')[1]    #Haal de eerste thumbnail eruit
+        request=urllib.request.Request(url,None,headers)                    
+        response = urllib.request.urlopen(request)                          
+        data = str(response.read())                                        
+        image_url = data.split('<a href="/url?q=')[1].split("src=")[1].split('"')[1]    
 
         urllib.request.urlretrieve(image_url, "img.jpg")
         return True
@@ -44,20 +50,19 @@ def get_result_google_thumb(search_term):
         return False
 
 def get_result_bing(search_term):
-    try:    #Probeer dit indien het foutgaat geef False terug anders True (zorgt dat de bot niet breekt)
-        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' #Deze header zorgt dat google niet gaat klagen dat we een bot zijn
+    try:    
+        user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7' 
 
-        headers={'User-Agent':user_agent,}                                  #Zet de header in elkaar
-
+        headers={'User-Agent':user_agent,}                                  
         url = "https://www.bing.com/images/search?q="\
-            + search_term.replace(" ", "+")                                 #Zoekveld voor bing, vervang spaties voor plustekens
+            + search_term.replace(" ", "+")                                 
 
-        request=urllib.request.Request(url,None,headers)                    #Stuur een http request
-        response = urllib.request.urlopen(request)                          #Vang het antwoord (locked het programma totdat het antwoord verkregen is, kan kapot gaan bij slechte verbinding!)
-        data = str(response.read())                                         #Lees het antwoord en zet het van bytecode om naar een tekst
+        request=urllib.request.Request(url,None,headers)                    
+        response = urllib.request.urlopen(request)                          
+        data = str(response.read())                                         
         with open('output.txt','w') as file:
             file.write(data)
-        image_url = data.split(".jpg&quot;,&quot;turl")[1].split("&quot;:&quot;")[-1]+".jpg" #Haal de afbeelding eruit. Dit gebeurt redelijk arbitrair :P
+        image_url = data.split(".jpg&quot;,&quot;turl")[1].split("&quot;:&quot;")[-1]+".jpg" 
 
         urllib.request.urlretrieve(image_url, "img.jpg")
         return True
